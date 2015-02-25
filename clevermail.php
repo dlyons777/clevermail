41f56604296e007232df24f2f72f5002f0c62825
7figureventures <dontrell@Macintosh.local>
2015-02-24 21:56:14 -0800

    Initial Commit

---
+++ clevermail.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Clever Mail
+Plugin URI: http://clevermail.com
+Description: Dopest mailing plugin EVER!
+Version: 1.0
+Author: Clever Digital Media
+Author URI: 
+License: 
+License URI: 
+*/
